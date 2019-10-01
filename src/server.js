9a63f9105c3412ff30a30dcbfb05acdf83588df2
Giancarlo <giancarlo.rau@pucp.pe>
2019-10-01 11:38:54 -0500

    update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+//app.use(express.static('dist'));
+const DIST_FOLDER = join(process.cwd(), 'dist');
 
 app.use(express.static(path.join(__dirname,'dist')));
 //app.use(express.static(__dirname/dist/));
@@ -10,7 +12,7 @@ app.use(express.static(path.join(__dirname,'dist')));
 //});
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname,'dist/index.html'));
+   res.sendFile(`./dist/index.html`);
 });
 
 
