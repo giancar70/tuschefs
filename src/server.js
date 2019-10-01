d66d21057598ae7cb66dca2f93d97936447ab767
Giancarlo <giancarlo.rau@pucp.pe>
2019-09-30 19:12:14 -0500

    update

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname/front-end/dist));
+app.use(express.static(__dirname/dist));
 app.get('*',(req,res) => {
-    res.sendFile(`./front-end/dist/index.html`);
+    res.sendFile(`./dist/index.html`);
 });
 
 app.listen(process.env.PORT || 8080);
