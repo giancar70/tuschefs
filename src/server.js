1bc02d50a5a7c2db6c5ab35c6e426cada40b352a
Giancarlo <giancarlo.rau@pucp.pe>
2019-10-01 11:11:22 -0500

    udpate

--- src/server.js
+++ src/server.js
@@ -3,9 +3,15 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname/dist/));
-app.get('*',(req,res) => {
-    res.sendFile(`./dist/index.html`);
+app.use(express.static(path.join(__dirname,'dist')));
+//app.use(express.static(__dirname/dist/));
+//app.get('*',(req,res) => {
+//    res.sendFile(`./dist/index.html`);
+//});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname,'dist/index.html'));
 });
+
 
 app.listen(process.env.PORT || 8080);
