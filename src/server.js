471c3fa95d8dc3b18d0f31a3872acd58ed5177f5
Giancarlo <giancarlo.rau@pucp.pe>
2019-10-01 19:16:37 -0500

    uodate

--- src/server.js
+++ src/server.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const path = require('path');
+//import appRoot from 'app-root-path';
 
 const app = express();
-//app.use(express.static('dist'));
-// const DIST_FOLDER = join(process.cwd(), 'dist');
+//app.use(express.static('./dist'));
+//const DIST_FOLDER = join(process.cwd(), 'dist');
+app.use(express.static('./dist'));
 
-app.use(express.static(path.join(__dirname,'dist')));
+
+//app.use(express.static(path.join(__dirname,'dist')));
 //app.use(express.static(__dirname/dist/));
 //app.get('*',(req,res) => {
 //    res.sendFile(`./dist/index.html`);
 //});
 
+//print(app.get('appPath'))
+
+console.log("path.join() : ", path.join());
+// outputs .
+console.log("path.resolve() : ", __dirname);
+console.log("path.resolve() : ", path.resolve(__dirname, '..'));
+
+
+//console.log("path.resolve() : ",path.resolve('dist','/dist'));
+
+
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname,'dist/index.html'));
+
+   res.sendFile(path.resolve(__dirname, '..') + '/dist/index.html');
+
 });
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
